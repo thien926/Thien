88fba996e80b95f7715c81aa9e4764580d9916e0
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2019-09-10 23:04:39 +0700

    Update hello.java

--- Java/Hoc javax.swing/hello.java
+++ Java/Hoc javax.swing/hello.java
@@ -1,23 +1,18 @@
 //https://www.youtube.com/watch?v=qZHp7NkO2nY&list=PLAxmMVqEIFdeN2-ql337WNwQZq7d3HDF5&index=3&t=0s
 package javaswing;
 
-import java.awt.Color;
 import javax.swing.*;
 
-public class labeldemo extends JFrame{
-    public labeldemo(){
-        setSize(400, 250);  // kich thuoc khung
+public class hello extends JFrame{
+    public hello(){
+        setSize(200, 150);  // kich thuoc khung
         setVisible(true);  // xuất khung
-        setLocation(500, 300); //xuất hiện khung ở vị trí giữa màn hình
+        setLocation(600, 300); //xuất hiện khung ở vị trí giữa màn hình
         setResizable(false);  //chọn thay kéo thay đỏi kích thước bảng
-        JLabel label = new JLabel("Study and Share");  //xuất chuỗi này
+        JLabel label = new JLabel("Hello World");
         add(label);
-        
-        label.setText("Việt Nam");  // xuất chuỗi
-        label.setToolTipText("A beautiful country");//chú thích khi rê con chuột đến
-        label.setForeground(Color.RED);
     }
     public static void main(String[] args){
-        labeldemo cc = new labeldemo();
+        hello cc = new hello();
     }
 }
