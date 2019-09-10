980fee251d8865ab9cff328348aeec2e92bad623
Thien Nguyen <52367055+thien926@users.noreply.github.com>
2019-09-10 23:03:59 +0700

    Create hello.java

---
+++ Java/Hoc javax.swing/hello.java
@@ -0,0 +1,23 @@
+//https://www.youtube.com/watch?v=qZHp7NkO2nY&list=PLAxmMVqEIFdeN2-ql337WNwQZq7d3HDF5&index=3&t=0s
+package javaswing;
+
+import java.awt.Color;
+import javax.swing.*;
+
+public class labeldemo extends JFrame{
+    public labeldemo(){
+        setSize(400, 250);  // kich thuoc khung
+        setVisible(true);  // xuất khung
+        setLocation(500, 300); //xuất hiện khung ở vị trí giữa màn hình
+        setResizable(false);  //chọn thay kéo thay đỏi kích thước bảng
+        JLabel label = new JLabel("Study and Share");  //xuất chuỗi này
+        add(label);
+        
+        label.setText("Việt Nam");  // xuất chuỗi
+        label.setToolTipText("A beautiful country");//chú thích khi rê con chuột đến
+        label.setForeground(Color.RED);
+    }
+    public static void main(String[] args){
+        labeldemo cc = new labeldemo();
+    }
+}
